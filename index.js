cce4aba0600e8a19ee7dfc3df4d26792e2d3c04b
Deniz <auo@tuta.io>
2025-07-18 19:03:39 +0200

    fetched coordinates from user locatin input

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 /*Test URL for 400 - 500 "https://jsonplaceholder.typicode.com/invalid-endpoint"*/
 const mainGridBox = document.getElementById("main-grid-box");
 
-
-
-
 /*BACKGROUND FOT THE BODY*/
 fetch("https://picsum.photos/v2/list?page=2&limit=100")
     .then((res) => {
@@ -18,36 +15,50 @@ fetch("https://picsum.photos/v2/list?page=2&limit=100")
         const randomIndex = Math.floor(Math.random() * data.length);
         //console.log(data[randomIndex].download_url);
         document.body.style.backgroundImage = `url(${data[randomIndex].download_url}`;
-    
+
         /*Display Phot info for Grid 4*/
-        console.log(data[randomIndex].author, data[randomIndex].url);
+        //console.log(data[randomIndex].author, data[randomIndex].url);
         //document.getElementById("photo-info").textContent = data[randomIndex].author;
-        
+
         const photoInfoDiv = document.getElementById("photo-info");
         const photoLink = document.createElement("a");
         photoLink.href = data[randomIndex].url;
         photoLink.textContent = data[randomIndex].author;
-    
+
         photoInfoDiv.append(photoLink);
-        
-    
-        
-    
     })
 
     .catch((err) => {
         console.log(err);
     });
 
+/*METEO FOR GRID 1*/
+const getLocBtn = document.getElementById("get-loc-btn");
+getLocBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const inputLocation = document.getElementById("input-location").value.toLowerCase().trim();
+    console.log(inputLocation);
+    fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${inputLocation}&count=1&language=en&format=json`)
+        .then((res) => {
+            //console.log(res);
+            if (!res.ok) {
+                throw new Error(`Error code: ${res.status}!`);
+            }
+            return res.json();
+        })
+        .then((data) => {
+            console.log(typeof data, data.results[0].latitude, data.results[0].longitude);
+        })
+        .catch((err) => console.error(err));
+});
+
 /*LOCAL TIME FOR GRID 2*/
 function currentTime() {
     const date = new Date();
-    
-    document.getElementById("current-time").textContent = date.toLocaleTimeString("en-es", {timeStyle: "medium"});
+
+    document.getElementById("current-time").textContent = date.toLocaleTimeString("en-es", { timeStyle: "medium" });
     document.getElementById("current-time").setAttribute("datetime", date);
 }
 
 setInterval(currentTime, 1000);
-
-
-
