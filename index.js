11bffc9aec3aef3130ad817d92250cfabcbafd49
Deniz <auo@tuta.io>
2025-07-20 10:12:41 +0200

    worked on changing meteo cities

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 /*Test URL for 400 - 500 "https://jsonplaceholder.typicode.com/invalid-endpoint"*/
 const mainGridBox = document.getElementById("main-grid-box");
+let city, region, country;
+let inputLocation = "Cadiz";
+
+
+
+/*Event Listeners*/
+
+const getLocBtn = document.getElementById("get-loc-btn");
+getLocBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    
+    inputLocation = document.getElementById("input-location").value.toLowerCase().trim();
+    displayMeteo(inputLocation);
+});
 
 /*BACKGROUND FOT THE BODY*/
 fetch("https://picsum.photos/v2/list?page=2&limit=100")
     .then((res) => {
-        //console.log(res);
-
         if (!res.ok) {
             throw new Error(`${res.status} Cannot connect to server!`);
         }
@@ -16,9 +28,7 @@ fetch("https://picsum.photos/v2/list?page=2&limit=100")
         //console.log(data[randomIndex].download_url);
         document.body.style.backgroundImage = `url(${data[randomIndex].download_url}`;
 
-        /*Display Phot info for Grid 4*/
-        //console.log(data[randomIndex].author, data[randomIndex].url);
-        //document.getElementById("photo-info").textContent = data[randomIndex].author;
+        /*Display Phot0 info for Grid 4*/
 
         const photoInfoDiv = document.getElementById("photo-info");
         const photoLink = document.createElement("a");
@@ -32,16 +42,11 @@ fetch("https://picsum.photos/v2/list?page=2&limit=100")
         console.log(err);
     });
 
-/*METEO FOR GRID 1*/
-const getLocBtn = document.getElementById("get-loc-btn");
-getLocBtn.addEventListener("click", function (e) {
-    e.preventDefault();
 
-    const inputLocation = document.getElementById("input-location").value.toLowerCase().trim();
-    let city, region, country;
-    
-    console.log(inputLocation);
-    fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${inputLocation}&count=5&language=en&format=json`)
+/*METEO FOR GRID 1*/
+
+function displayMeteo(loc) {
+    fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${loc}&count=5&language=en&format=json`)
         .then((res) => {
             //console.log(res);
             if (!res.ok) {
@@ -54,7 +59,7 @@ getLocBtn.addEventListener("click", function (e) {
             const lat = data.results[0].latitude;
             const lon = data.results[0].longitude;
             city = data.results[0].name;
-        region = data.results[0].admin1;
+            region = data.results[0].admin1;
             country = data.results[0].country_code;
             return fetch(
                 "https://api.open-meteo.com/v1/forecast?" +
@@ -74,13 +79,15 @@ getLocBtn.addEventListener("click", function (e) {
             const minTemp = document.getElementById("min-temp");
             const maxTemp = document.getElementById("max-temp");
 
-        cityName.textContent = city + "," + region + "," + country;
-        meanTemp.textContent = meteo.daily.temperature_2m_mean[0];
-        minTemp.textContent = meteo.daily.temperature_2m_min[0];
-        maxTemp.textContent = meteo.daily.temperature_2m_max[0];
+            cityName.textContent = city + "," + region + "," + country;
+            meanTemp.textContent = meteo.daily.temperature_2m_mean[0];
+            minTemp.textContent = meteo.daily.temperature_2m_min[0];
+            maxTemp.textContent = meteo.daily.temperature_2m_max[0];
         })
         .catch((err) => console.error(err));
-});
+}
+
+displayMeteo(inputLocation);
 
 /*LOCAL TIME FOR GRID 2*/
 function currentTime() {
